a80e921d09b975e6f5357fcd2475fb11000d8b54
Данил Змиевский <danilzmievskiy@MacBook-Air-Danil.local>
2022-09-23 01:06:32 +0300

    Friends page and requests

--- src/users/views.py
+++ src/users/views.py
@@ -3,13 +3,14 @@ from .forms import SpectatorCreationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import JsonResponse
 
 from .models import Spectator
 from tracker.models import Film
+from friendship.models import Friend, FriendshipRequest
 
 
 def user_detail(request, pk):
-    login_user = request.user
     user = Spectator.objects.get(pk=pk)
     req_user = request.user
     films = list(user.films.keys())
@@ -29,12 +30,65 @@ def user_detail(request, pk):
             quit.append({'pk': film,
                             'name': Film.objects.get(pk=films[0]).name})
 
+    rqs = Friend.objects.sent_requests(user=request.user)
+    print(rqs)
+
     return render(request, 'user_detail.html', context={'user': user,
-                                                        'login_user': login_user,
                                                         'watched': watched,
                                                         'watching': watching,
                                                         'plan': plan,
-                                                        'quit': quit})
+                                                        'quit': quit,
+                                                        'rqs': rqs})
+
+
+def friends_detail(request):
+    friends = Friend.objects.friends(request.user)
+    requests = Friend.objects.unrejected_requests(user=request.user)
+    return render(request, 'friends.html', context={'friends': friends,
+                                                    'requests': requests})
+
+
+def friends_search(request):
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        res = None
+        search_user = request.POST.get('username')
+        obj = Spectator.objects.filter(username__icontains=search_user)
+        if len(obj) > 0 and len(search_user) > 0:
+            data = []
+            for pos in obj[:4]:
+                item = {
+                    'pk': pos.pk,
+                    'name': pos.username
+                }
+                data.append(item)
+            res = data
+        else:
+            res = 'Nothing found ...'
+        return JsonResponse({'data': res})
+    return JsonResponse({})
+
+
+def friend_request(request, pk):
+    other_user = Spectator.objects.get(pk=pk)
+    Friend.objects.add_friend(
+        request.user,
+        other_user,
+        message="Привет! Будь моим семпаем!")
+    return user_detail(request, pk)
+
+
+def accept_request(request, pk):
+    other_user = Spectator.objects.get(pk=pk)
+    friend_request = FriendshipRequest.objects.get(from_user=other_user, to_user=request.user)
+    friend_request.accept()
+    return friends_detail(request)
+
+
+def reject_request(request, pk):
+    other_user = Spectator.objects.get(pk=pk)
+    friend_request = FriendshipRequest.objects.get(from_user=other_user, to_user=request.user)
+    friend_request.reject()
+    return friends_detail(request)
 
 
 def register_request(request):
