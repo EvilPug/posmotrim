5061bfdb306d637b574c067a1de236fddf4e2c04
Данил Змиевский <danilzmievskiy@MacBook-Air-Danil.local>
2022-09-23 20:38:50 +0300

    Change film label method

--- src/tracker/urls.py
+++ src/tracker/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search_results, name='search'),
     path('film/<int:pk>', views.film_detail, name='film-detail'),
+    path('watching/<int:pk>', views.tag_film, name='watching_tag'),
+    path('watched/<int:pk>', views.tag_film, name='watched_tag'),
+    path('plan/<int:pk>', views.tag_film, name='plan_tag'),
+    path('quit/<int:pk>', views.tag_film, name='quit_tag'),
+
 ]
